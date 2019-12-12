b341a86612b8856693a16ae78e622c4fa28b6cc5
=Nguyen Hai <=haivnguyen1999@gmail.com4>
2019-12-12 08:16:30 +0700

    commit lab 9 final

--- build/generated-sources/ap-source-output/entity/OrderedProduct_.java
+++ build/generated-sources/ap-source-output/entity/OrderedProduct_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-12-11T18:06:57")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-12-12T08:04:33")
 @StaticMetamodel(OrderedProduct.class)
 public class OrderedProduct_ { 
 
--- src/java/listener/ControllerServletListener.java
+++ src/java/listener/ControllerServletListener.java
@@ -1,41 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package listener;
-
 import javax.ejb.EJB;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import session_bean.CategorySessionBean;
 import session_bean.ProductSessionBean;
-
-/**
- * Web application lifecycle listener.
- *
- * @author hai.nv173089
- */
 public class ControllerServletListener implements ServletContextListener {
-    
     @EJB
     ProductSessionBean productSessionBean;
-    
+    @EJB
+    CategorySessionBean categorySessionBean;  
     ServletContext context;
-    
-    
-
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         context = sce.getServletContext();
+        context.setAttribute("categories", categorySessionBean.findRange(new int[] {0,5}));
         context.setAttribute("newProducts", productSessionBean.findRange(new int[] {0,5}));
-        int x=5;
     }
-
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
         this.context = null;
     }
-
 }
 
